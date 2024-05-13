06dc0e2eaa4d0b49b97a39bb043d759666e0b941
apluzhnik <apluzhnik@DJ-PC-K209-10>
2024-05-13 17:01:52 +0300

    Добавьте файлы проекта.

---
+++ meow/Core/ObservableObjectBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace design.Core
+{
+    internal class ObservableObjectBase
+    {
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+    }
+}
---
+++ meow/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using design.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+        public HomeViewModel HomeMV { get; set; }
+        public DiscoveryViewModel DiscoveryMV { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeMV = new HomeViewModel();
+            DiscoveryMV = new DiscoveryViewModel();
+            CurrentView = HomeMV;
+
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeMV;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryMV;
+            });
+        }
+    }
+}
