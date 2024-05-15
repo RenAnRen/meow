c38df625fbccce79301fdbc2280504f4788b5cb4
anastasia.pluzhnik@outlook.com <anastasia.pluzhnik@outlook.com>
2024-05-15 16:14:31 +0300

    день недели

--- meow/MVVM/ViewModel/HomeViewModel.cs
+++ meow/MVVM/ViewModel/HomeViewModel.cs
@@ -8,6 +8,6 @@ namespace design.MVVM.ViewModel
 {
     internal class HomeViewModel
     {
-
+        
     }
 }
--- meow/MainWindow.xaml.cs
+++ meow/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using design.MVVM.View;
+using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace design
 {
@@ -20,11 +24,56 @@ namespace design
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            sidepanel.Visibility = Visibility.Hidden;
+
+            HomeView homeView = new HomeView();
+          
+            homeView.dfw.Text = DateTime.Now.DayOfWeek.ToString();
+            MessageBox.Show(homeView.dfw.Text);
         }
 
-     
+    
+        private void panelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+
+            sidepanel.Visibility = Visibility.Visible;
+            panelHeader.Visibility = Visibility.Hidden;
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+          
+            sidepanel.Visibility = Visibility.Hidden;
+            panelHeader.Visibility = Visibility.Visible;
+        }
+
+      
+
+        //private static void DayOfWeek()
+        //{
+        //    DateTime date = DateTime.Now;
+        //    string day = date.ToString("dddd");
+        //    //   HomeView HVM = new HomeView();
+        //    string DAY = day.ToUpper();
+        //    string aaa = DAY + ", " + $"{date:M}";
+        //    MainWindow hmv = new MainWindow();
+        //    hmv.Show();
+
+        //    hmv.DataContext = aaa;
+
+        //    //HVM.dfw.Text = day + ", " + $"{date:M}";
+        //}
+
     }
 }
