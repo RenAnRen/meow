7db8704a8873fbd66239b08ab7e17b4b5efe39fc
anastasia.pluzhnik@outlook.com <anastasia.pluzhnik@outlook.com>
2024-05-24 16:18:08 +0300

    фильтры

--- meow/MVVM/View/ContractorView.xaml.cs
+++ meow/MVVM/View/ContractorView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace design.MVVM.View
 {
@@ -20,9 +22,32 @@ namespace design.MVVM.View
     /// </summary>
     public partial class ContractorView : UserControl
     {
+        string SQL = "Server=yurilo-9916.7tc.aws-eu-central-1.cockroachlabs.cloud;Port=26257;Database=yurilo_db; User Id = yurilo_admin; Password=yrPjMJoa5LQZGO1owOKdNA;";
+
+
         public ContractorView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //NpgsqlConnection sqlConnection = new NpgsqlConnection(SQL);
+            //sqlConnection.Open();
+
+            //NpgsqlCommand command = new NpgsqlCommand();
+            //command.Connection = sqlConnection;
+            //command.CommandType = CommandType.Text;
+
+            //NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter();
+            //DataTable dataTable = new DataTable("yurilo_db.counterparties");
+            //dataAdapter.SelectCommand = command;
+            //command.CommandText = "INSERT INTO yurilo_db.counterparties (name, id_subject_type, address, number, company, INN, KPP, OGRN, email, notes) VALUES ('" + name.Text + "','" + subject_type.Text + "','" + address.Text + "','" + number.Text + "','" + company.Text + "','" + INN.Text + "','" + KPP.Text + "','" + OGRN.Text + "','" + email.Text + "','" + notes.Text + "');";
+
+            ////command.CommandText = "SELECT * FROM users WHERE login='" + log.Text + "' AND password ='" + passw.Password + "';";
+            //NpgsqlDataReader dr = command.ExecuteReader();
+
+
         }
     }
 }
--- meow/MVVM/View/MeetsView.xaml.cs
+++ meow/MVVM/View/MeetsView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +22,81 @@ namespace design.MVVM.View
     /// </summary>
     public partial class MeetsView : UserControl
     {
+        string SQL = "Server=yurilo-9916.7tc.aws-eu-central-1.cockroachlabs.cloud;Port=26257;Database=yurilo_db; User Id = yurilo_admin; Password=yrPjMJoa5LQZGO1owOKdNA;";
+
+
         public MeetsView()
         {
             InitializeComponent();
+
+            ComboBoxes();
+
         }
 
-      
+        private void ComboBoxes() 
+        {
+            ListContr();
+            ListServ();
+        }
+
+        private void ListContr()
+        {
+           NpgsqlConnection sqlConnection = new NpgsqlConnection(SQL);
+            sqlConnection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter();
+            DataTable dataTable = new DataTable("yurilo_db.counterparties");
+            DataSet ds = new DataSet();
+            dataAdapter.SelectCommand = command;
+
+            command.CommandText = "SELECT name FROM yurilo_db.counterparties;";
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            while (dr.Read())
+            {
+                comboBoxcont.Items.Add(dr["Name"].ToString());
+            }
+ 
+            command.Dispose();
+            sqlConnection.Close();  
+        }
+
+        private void ListServ()
+        {
+            NpgsqlConnection sqlConnection = new NpgsqlConnection(SQL);
+            sqlConnection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter();
+            DataTable dataTable = new DataTable("yurilo_db.counterparties");
+            DataSet ds = new DataSet();
+            dataAdapter.SelectCommand = command;
+
+            command.CommandText = "SELECT descriptions FROM yurilo_db.services;";
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            while (dr.Read())
+            {
+                comboBoxserv.Items.Add(dr["descriptions"].ToString());
+            }
+
+            command.Dispose();
+            sqlConnection.Close();
+        }
+
+        private void Label_GotFocus(object sender, RoutedEventArgs e)
+        {
+            comboBoxserv.SelectedIndex = -1;
+            comboBoxcont.SelectedIndex = -1;
+            Date.Text = "ДД.ММ.ГГГГ";
+            comboBox.SelectedIndex = -1;    
+        }
     }
 }
